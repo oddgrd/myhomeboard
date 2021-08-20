8a3f03c7543d8f975472528c170dcb95940a6cdd
Oddgrd <oddgrd@gmail.com>
2021-08-20 18:36:49 +0200

    express-session with redisstore setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,32 @@
 import 'reflect-metadata';
-import express from "express";
+import 'dotenv-safe/config';
+import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
-import { TestResolver } from "./resolvers/testResolver";
+import { TestResolver } from './resolvers/testResolver';
 import cors from 'cors';
+import { createConnection } from 'typeorm';
+import { Board } from './entities/Board';
+import { Problem } from './entities/Problem';
+import { User } from './entities/User';
+import path from 'path';
+import Redis from 'ioredis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import { UserResolver } from './resolvers/userResolver';
 
 const main = async () => {
+  const connection = await createConnection({
+    applicationName: 'covegg19_v2',
+    type: 'postgres',
+    database: 'covegg19_1',
+    username: 'postgres',
+    password: 'postgres',
+    entities: [User, Problem, Board],
+    migrations: [path.join(__dirname, './migrations/*')],
+    logging: true,
+    synchronize: true
+  });
   const app = express();
 
   const devWhitelist = [
@@ -25,26 +46,46 @@ const main = async () => {
       credentials: true
     })
   );
-  
+
+  const RedisStore = connectRedis(session);
+  const redis = new Redis();
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [TestResolver],
+      resolvers: [TestResolver, UserResolver],
       validate: false
     }),
-    context: ({req, res}) => ({
+    context: ({ req, res }) => ({
       req,
-      res
+      res,
+      redis
     })
-  })
-  await apolloServer.start();
-  apolloServer.applyMiddleware({app, cors: false});
+  });
 
-  app.get("/", (_req, res) => {
-    res.send("Hello World!")
-  })
+  app.use(
+    session({
+      name: 'c19',
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      store: new RedisStore({ client: redis, disableTouch: true }),
+      cookie: {
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'none',
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10
+      }
+    })
+  );
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  app.get('/', (_req, res) => {
+    res.send('Hello World!');
+  });
   app.listen(4000, () => {
-    console.log("App listening on port 4000")
-  })
+    console.log('App listening on port 4000');
+  });
 };
 
 main().catch((err) => {
