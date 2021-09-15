d7820465fea22651f40b5ab51ba1678f8aed1e93
Oddgrd <oddgrd@gmail.com>
2021-09-15 15:56:16 +0200

    Set dpr dynamically

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
 import styles from '../styles/Canvas.module.scss';
+import { isServer } from '../utils/isServer';
 interface Props {
   canvasRef?: React.MutableRefObject<HTMLCanvasElement | undefined>;
 }
 
 export const Canvas = ({ canvasRef }: Props) => {
+  const ratio = (!isServer() && Math.ceil(window.devicePixelRatio)) || 1;
   return (
     <div className={styles.container}>
       <canvas
@@ -19,9 +21,7 @@ export const Canvas = ({ canvasRef }: Props) => {
         className={styles.image}
         width={350}
         height={478}
-        src={
-          'https://res.cloudinary.com/dqyhbqh0x/image/upload/c_scale,h_478,q_100,w_360/v1628264532/covegg19-0_1_0_ziflc1.jpg'
-        }
+        src={`https://res.cloudinary.com/dqyhbqh0x/image/upload/c_scale,h_478,q_100,w_360,dpr_${ratio}.0/v1628264532/covegg19-0_1_0_ziflc1.jpg`}
         alt='User Avatar'
       />
     </div>
