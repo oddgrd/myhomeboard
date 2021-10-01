cf480d1a23b65ced5637f550c25ad9e0e0abeaeb
Oddgrd <oddgrd@gmail.com>
2021-10-01 22:00:13 +0200

    Style toastContainer, clear problems on prod migration

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, Zoom } from 'react-toastify';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ToastContainer />
+      <ToastContainer theme='dark' />
       <Component {...pageProps} />
     </>
   );
