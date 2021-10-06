d41dc046e106a72390c396613b357703dcbfc47d
Oddgrd <oddgrd@gmail.com>
2021-10-06 14:42:41 +0200

    First iteration of security headers

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,49 @@
+const { nodeModuleNameResolver } = require('typescript');
+
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "Content-Security-Policy",
+    value:
+      "default-src 'self' *.myhomeboard.no; connect-src 'self' *.myhomeboard.no vitals.vercel-insights.com; img-src 'self' lh3.googleusercontent.com res.cloudinary.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;",
+  },
+];
+
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['lh3.googleusercontent.com', 'res.cloudinary.com']
-  }
+    domains: ["lh3.googleusercontent.com", "res.cloudinary.com"],
+  },
+  async headers() {
+    return process.env.NODE_ENV === "production" ? [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ] : [];
+  },
 };
