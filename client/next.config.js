05b696b23a7d9f17a404e2ebb88c2e2670c67468
Oddgrd <oddgrd@gmail.com>
2021-10-07 20:02:40 +0200

    Reformat with single quotes

--- client/next.config.js
+++ client/next.config.js
@@ -1,49 +1,48 @@
-const { nodeModuleNameResolver } = require('typescript');
-
 /** @type {import('next').NextConfig} */
 const securityHeaders = [
   {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
   },
   {
-    key: "X-DNS-Prefetch-Control",
-    value: "on",
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
   },
   {
-    key: "X-XSS-Protection",
-    value: "1; mode=block",
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
   },
   {
-    key: "X-Frame-Options",
-    value: "SAMEORIGIN",
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
   },
   {
-    key: "Permissions-Policy",
-    value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
   },
   {
-    key: "Referrer-Policy",
-    value: "origin-when-cross-origin",
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
   },
   {
-    key: "Content-Security-Policy",
-    value:
-      "default-src 'self'; connect-src 'self' https://api.myhomeboard.no vitals.vercel-insights.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' lh3.googleusercontent.com res.cloudinary.com data: https:; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; object-src 'none'",
-  },
+    key: 'Content-Security-Policy',
+    value: `default-src 'self'; connect-src 'self' https://api.myhomeboard.no vitals.vercel-insights.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' lh3.googleusercontent.com res.cloudinary.com data: https:; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; object-src 'none'`
+  }
 ];
 
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["lh3.googleusercontent.com", "res.cloudinary.com"],
+    domains: ['lh3.googleusercontent.com', 'res.cloudinary.com']
   },
   async headers() {
-    return process.env.NODE_ENV === "production" ? [
-      {
-        source: "/(.*)",
-        headers: securityHeaders,
-      },
-    ] : [];
-  },
+    return process.env.NODE_ENV === 'production'
+      ? [
+          {
+            source: '/(.*)',
+            headers: securityHeaders
+          }
+        ]
+      : [];
+  }
 };
