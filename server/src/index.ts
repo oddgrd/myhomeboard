420010ede9a07a62bdf3e06c16ef97fce6516a0c
Oddgrd <oddgrd@gmail.com>
2021-10-11 19:28:29 +0200

    Test GH actions

--- server/src/index.ts
+++ server/src/index.ts
@@ -25,8 +25,6 @@ import { UserResolver } from './resolvers/userResolver';
 import authRoutes from './routes/api/auth';
 import { createUserLoader } from './utils/createUserLoader';
 
-// Test github actions deploy workflow
-
 const main = async () => {
   const connection = await createConnection({
     applicationName: 'myhomeboard',
