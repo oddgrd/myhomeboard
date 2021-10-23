28266b97759431dcf505aee7f78bcfb4833847f2
Oddgrd <oddgrd@gmail.com>
2021-10-23 23:47:30 +0200

    Jest config

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,8 +1,18 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const TEST_REGEX = '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$';
+
 module.exports = {
-  rootDir: 'src',
+  setupFilesAfterEnv: [`${__dirname}/jest.setup.ts`],
+  testRegex: TEST_REGEX,
+  moduleNameMapper: {
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transform: {
+    '\\.svg$': '<rootDir>/jest-svg-transformer.js',
+  },
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.jest.json',
