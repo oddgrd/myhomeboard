90278ab6cad6eb1f2cbf8a9796f67e693e55ca87
Oddgrd <oddgrd@gmail.com>
2021-11-12 13:25:45 +0100

    iOS PWA config

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -18,8 +18,28 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <link rel='manifest' href='/manifest.json' />
-          <link rel='apple-touch-icon' href='/favicon.png'></link>
+          <link rel='apple-touch-icon' href='/favicon.png' />
+          <link
+            rel='apple-touch-icon'
+            sizes='152x152'
+            href='/icons/logo152.png'
+          />
+          <link
+            rel='apple-touch-icon'
+            sizes='192x192'
+            href='/icons/logo192.png'
+          />
+          <link
+            rel='apple-touch-icon'
+            sizes='512x512'
+            href='/icons/logo512.png'
+          />
           <meta name='theme-color' content='#1c1c31' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta
+            name='apple-mobile-web-app-status-bar-style'
+            content='black-translucent'
+          />
         </Head>
         <body>
           <Main />
